c07fce9b25884fd81a04adddcf19ba7c644efc5d
Burkhard Graves <graves@id-on.de>
2019-05-09 16:37:24 +0200

    first commit

---
+++ webapp/src/main/java/de/idon/sandbox/SandboxJspApplication.java
@@ -0,0 +1,12 @@
+package de.idon.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxJspApplication.class, args);
+	}
+}
